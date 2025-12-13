6c6ac9871f756de23e4285260147a524ffc3fe21
Agasthyahm <hmagasthya@gmail.com>
2025-12-13 12:44:42 +0530

    Update CalculatorTest.java

--- src/test/java/com/example/CalculatorTest.java
+++ src/test/java/com/example/CalculatorTest.java
@@ -13,7 +13,7 @@ public class CalculatorTest {
         assertEquals(0, calc.add(-2, 2));
     }
 
-    /*@Test
+    @Test
     void testSubtract() {
         assertEquals(1, calc.subtract(3, 2));
         assertEquals(-4, calc.subtract(-2, 2));
@@ -24,5 +24,5 @@ public class CalculatorTest {
         assertTrue(calc.isPositive(5));
         assertFalse(calc.isPositive(-1));
         assertFalse(calc.isPositive(0));
-    }*/
+    }
 }
